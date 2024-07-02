ee81a89598316b14a58008b465e0a0a3e1651d46
Samodh Fernando <108604168+samodhfdo@users.noreply.github.com>
2024-07-01 23:08:57 -0700

    Add files via upload

---
+++ Temperature.py
@@ -0,0 +1,6 @@
+x = input("Specify the rquired temperature in Celcius: ")
+
+temp1 = int(x)
+
+print(f"The temperature in Farenheit is {temp1 * 9/5 + 32}")
+print(f"The temperature in Kelvin is {temp1 + 273.15}")
